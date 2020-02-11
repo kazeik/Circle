07768dc92fb2482b6de8ed4b39f67716cd71790c
kazeik <kazeik@163.com>
2020-02-11 17:45:12 +0800

    修正页面缓存的问题

--- lib/page/PageOne.dart
+++ lib/page/PageOne.dart
@@ -5,36 +5,27 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:fluttertest/model/ImageModel.dart';
-import 'package:fluttertest/model/NewsListItemModel.dart';
-import 'package:fluttertest/model/NewsModel.dart';
 import 'package:fluttertest/model/TabsModel.dart';
 import 'package:fluttertest/net/HttpNet.dart';
 import 'package:fluttertest/utils/ApiUtils.dart';
 import 'package:fluttertest/utils/MethodTyps.dart';
-import 'package:fluttertest/utils/Utils.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:fluttertest/weight/TabViewList.dart';
 
 class PageOne extends StatefulWidget {
   State<StatefulWidget> createState() => new _PageOneState();
 }
 
-class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
+class _PageOneState extends State<PageOne> with TickerProviderStateMixin {
   TabController _mController;
   List<Tab> _tabList = new List();
   TabsModel _model;
-  int _pageIndex = 1;
+
   int _tabId;
-  bool pullUp = true;
-
-  List<List<NewsListItemModel>> allData = new List();
-
-  RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
 
   @override
   void initState() {
     super.initState();
+    _mController = new TabController(length: 0, vsync: this);
     _getTabs();
   }
 
@@ -47,7 +38,6 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
   _getTabs() {
     HttpNet().request(MethodTypes.GET, ApiUtils.getTabs, (str) {
       _model = TabsModel.fromJson(str);
-      _tabList.clear();
       _model.data.list.forEach((item) {
         _tabList.add(new Tab(text: item.classname));
       });
@@ -57,13 +47,11 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
         vsync: this,
       );
       _tabId = _model?.data?.list[0].id;
-      allData = new List(_model?.data?.list?.length);
       _mController.addListener(() {
         _tabId = _model?.data?.list[_mController.index].id;
-        _pageIndex = 1;
-        _getListData(clear: true);
+        _reflashWidget();
       });
-      _getListData();
+      _reflashWidget();
     });
   }
 
@@ -97,68 +85,18 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
   }
 
   Widget _buildView() {
-    if (_tabList.isNotEmpty) {
+    if (_tabList == null || _tabList.isEmpty) {
+      return new Container();
+    } else {
       return TabBarView(
         controller: _mController,
         children: _tabList.map((item) {
-          return SmartRefresher(
-            enablePullDown: true,
-            enablePullUp: pullUp,
-            header: ClassicHeader(
-                refreshingText: "重新加载数据中",
-                releaseText: "松开时重新加载数据",
-                completeText: "数据加载完成",
-                idleText: "下拉加载新数据"),
-            controller: _refreshController,
-            onRefresh: _onRefresh,
-            onLoading: _onLoading,
-            child: new ListView.builder(
-                itemCount: allData[_mController.index] == null ||
-                        allData[_mController.index].isEmpty
-                    ? 0
-                    : allData[_mController.index].length,
-                itemBuilder: _buildItemList),
+          return TabViewList(
+            tabId: _tabId,
           );
         }).toList(),
       );
-    } else {
-      return new Container();
     }
-  }
-
-  void _onRefresh() async {
-    _pageIndex = 1;
-    _getListData(clear: true);
-  }
-
-  void _onLoading() async {
-    _pageIndex++;
-    _getListData();
-  }
-
-  _getListData({bool clear = false}) {
-    HttpNet().request(
-        MethodTypes.GET, "${ApiUtils.getTabs}/$_tabId/post/$_pageIndex", (str) {
-      NewsModel model = NewsModel.fromJson(str);
-//      if (clear) {
-//        allData.clear();
-//      }
-      Utils.logs("当前索引 ： ${_mController.index}");
-      if (model.data == null ||
-          model.data.list == null ||
-          model.data.list.isEmpty) {
-        pullUp = false;
-        Utils.showToast("没有更多数据了");
-      } else {
-        if (allData[_mController.index] == null) {
-          allData[_mController.index] = new List();
-        }
-        allData[_mController.index].addAll(model.data.list);
-      }
-      _reflashWidget();
-      _refreshController.refreshCompleted();
-      _refreshController.loadComplete();
-    });
   }
 
   Widget _buildTabs() {
@@ -178,9 +116,6 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
         }).toList(),
         onTap: (index) {
           _tabId = _model?.data?.list[index].id;
-          _pageIndex = 1;
-          pullUp = true;
-          _getListData(clear: true);
         },
       );
     } else {
@@ -190,171 +125,5 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
 
   _reflashWidget() {
     if (mounted) setState(() {});
-  }
-
-  Widget _buildItemList(BuildContext context, int index) {
-    List<NewsListItemModel> tempModel = allData[_mController.index];
-    if (tempModel == null || tempModel.isEmpty)
-      return new Container();
-    else {
-      return new Container(
-        color: Colors.white,
-        child: new Column(
-          children: <Widget>[
-            new ListTile(
-              leading: new ClipOval(
-                child: new Image(
-                  width: 45,
-                  height: 45,
-                  image: new NetworkImage(tempModel[index].user?.userpic),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              title: new Text(
-                "${tempModel[index].user?.realname}",
-                style: new TextStyle(
-                    fontSize: 16,
-                    color: const Color(0xff6f6f6f),
-                    fontWeight: FontWeight.bold),
-              ),
-              subtitle: new Text(
-                "${_getTime(tempModel[index]?.create_time)}",
-                style: new TextStyle(color: Colors.black38, fontSize: 12),
-              ),
-              trailing: new Container(
-                margin: EdgeInsets.only(left: 10, right: 10),
-                alignment: Alignment.center,
-                height: 25,
-                width: 80,
-                decoration: new BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.all(Radius.circular(14.0)),
-                  border: new Border.all(width: 1, color: Colors.teal),
-                ),
-                child: new Text(
-                  "测试",
-                  style: new TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            new Container(
-              margin: EdgeInsets.all(15),
-              alignment: Alignment.centerLeft,
-              child: new Text("${tempModel[index]?.content}"),
-            ),
-            _buildImgItem(index),
-            new Divider(
-              indent: 10,
-              endIndent: 10,
-            ),
-            new Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: _buildRowItem(index),
-              ),
-            ),
-            new Container(
-              height: 15,
-              color: Colors.black12,
-            )
-          ],
-        ),
-      );
-    }
-  }
-
-  String _getTime(int time) {
-    var daynow = DateTime.fromMillisecondsSinceEpoch(time * 1000);
-    Duration chaf = DateTime.now().difference(daynow);
-    if (chaf.inDays != 0) {
-      if (chaf.inDays > 30) {
-        return "${daynow.year}-${daynow.month}-${daynow.day}";
-      } else
-        return "${chaf.inDays}天前";
-    } else if (chaf.inHours != 0) {
-      return "${chaf.inHours}时前";
-    } else if (chaf.inMinutes != 0) {
-      return "${chaf.inMinutes}分前";
-    } else if (chaf.inSeconds != 0) {
-      return "${chaf.inSeconds}刚刚";
-    }
-  }
-
-  List<Widget> _buildRowItem(int index) {
-    List<Widget> all = new List();
-    all.add(new Row(
-      children: <Widget>[
-        new Icon(Icons.apps),
-        new Padding(
-          padding: EdgeInsets.only(left: 5),
-          child: new Text(
-              "${allData[_mController.index][index]?.ding_count} Like"),
-        )
-      ],
-    ));
-    all.add(new Row(
-      children: <Widget>[
-        new Icon(Icons.comment),
-        new Padding(
-          padding: EdgeInsets.only(left: 5),
-          child: new Text(
-              "${allData[_mController.index][index]?.comment_count} Comment"),
-        )
-      ],
-    ));
-    all.add(new Row(
-      children: <Widget>[
-        new Icon(Icons.share),
-        new Padding(
-          padding: EdgeInsets.only(left: 5),
-          child:
-              new Text("${allData[_mController.index][index]?.share_id} Share"),
-        )
-      ],
-    ));
-    return all;
-  }
-
-  // ignore: missing_return
-  Widget _buildImgItem(int index) {
-    List<ImageModel> allImgs = allData[_mController.index][index]?.images;
-    var length = allImgs?.length;
-    if (length == 1) {
-      return new Container(
-        width: double.infinity,
-        height: 200,
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: new NetworkImage(allImgs[0]?.url),
-          ),
-        ),
-      );
-    } else if (length == 0) {
-      return new Container();
-    } else {
-      return _buidGridView(allImgs);
-    }
-  }
-
-  Widget _buidGridView(List<ImageModel> imgs) {
-    return new GridView.builder(
-      shrinkWrap: true,
-      itemCount: imgs.length,
-      padding: EdgeInsets.all(5.0),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        mainAxisSpacing: 5.0,
-        crossAxisSpacing: 5.0,
-        crossAxisCount: imgs.length <= 3 ? imgs.length : 3,
-        childAspectRatio: 1.0,
-      ),
-      physics: NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) {
-        return new Image(
-          image: new NetworkImage(imgs[index].url),
-          fit: BoxFit.fill,
-        );
-      },
-    );
   }
 }
---
+++ lib/weight/TabViewList.dart
@@ -0,0 +1,259 @@
+/*
+ * @author jingsong.chen, QQ:77132995, email:kazeik@163.com
+ * 2020-02-11 16:09
+ * 类说明:
+ */
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertest/model/ImageModel.dart';
+import 'package:fluttertest/model/NewsListItemModel.dart';
+import 'package:fluttertest/model/NewsModel.dart';
+import 'package:fluttertest/net/HttpNet.dart';
+import 'package:fluttertest/utils/ApiUtils.dart';
+import 'package:fluttertest/utils/MethodTyps.dart';
+import 'package:fluttertest/utils/Utils.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+class TabViewList extends StatefulWidget {
+  final int tabId;
+
+  const TabViewList({Key key, this.tabId}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => new _TabViewListState();
+}
+
+class _TabViewListState extends State<TabViewList>
+    with AutomaticKeepAliveClientMixin {
+  bool pullUp = true;
+  int _pageIndex = 1;
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  List<NewsListItemModel> allData = new List();
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: pullUp,
+        header: ClassicHeader(
+            refreshingText: "重新加载数据中",
+            releaseText: "松开时重新加载数据",
+            completeText: "数据加载完成",
+            idleText: "下拉加载新数据"),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: new ListView.builder(
+            itemCount: allData == null || allData.isEmpty ? 0 : allData.length,
+            itemBuilder: _buildItemList),
+      ),
+    );
+  }
+
+  void _onRefresh() async {
+    _pageIndex = 1;
+    _getListData(clear: true);
+  }
+
+  void _onLoading() async {
+    _pageIndex++;
+    _getListData();
+  }
+
+  Widget _buildItemList(BuildContext context, int index) {
+    if (allData == null)
+      return new Container();
+    else {
+      return new Container(
+        color: Colors.white,
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
+              leading: new ClipOval(
+                child: new Image(
+                  width: 45,
+                  height: 45,
+                  image: new NetworkImage(allData[index].user?.userpic),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              title: new Text(
+                "${allData[index].user?.realname}",
+                style: new TextStyle(
+                    fontSize: 16,
+                    color: const Color(0xff6f6f6f),
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: new Text(
+                "${_getTime(allData[index]?.create_time)}",
+                style: new TextStyle(color: Colors.black38, fontSize: 12),
+              ),
+              trailing: new Container(
+                margin: EdgeInsets.only(left: 10, right: 10),
+                alignment: Alignment.center,
+                height: 25,
+                width: 80,
+                decoration: new BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.all(Radius.circular(14.0)),
+                  border: new Border.all(width: 1, color: Colors.teal),
+                ),
+                child: new Text(
+                  "测试",
+                  style: new TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            new Container(
+              margin: EdgeInsets.all(15),
+              alignment: Alignment.centerLeft,
+              child: new Text("${allData[index]?.content}"),
+            ),
+            _buildImgItem(index),
+            new Divider(
+              indent: 10,
+              endIndent: 10,
+            ),
+            new Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: _buildRowItem(index),
+              ),
+            ),
+            new Container(
+              height: 15,
+              color: Colors.black12,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    _getListData();
+    super.initState();
+  }
+
+  List<Widget> _buildRowItem(int index) {
+    List<Widget> all = new List();
+    all.add(new Row(
+      children: <Widget>[
+        new Icon(Icons.apps),
+        new Padding(
+          padding: EdgeInsets.only(left: 5),
+          child: new Text("${allData[index]?.ding_count} Like"),
+        )
+      ],
+    ));
+    all.add(new Row(
+      children: <Widget>[
+        new Icon(Icons.comment),
+        new Padding(
+          padding: EdgeInsets.only(left: 5),
+          child: new Text("${allData[index]?.comment_count} Comment"),
+        )
+      ],
+    ));
+    all.add(new Row(
+      children: <Widget>[
+        new Icon(Icons.share),
+        new Padding(
+          padding: EdgeInsets.only(left: 5),
+          child: new Text("${allData[index]?.share_id} Share"),
+        )
+      ],
+    ));
+    return all;
+  }
+
+  // ignore: missing_return
+  Widget _buildImgItem(int index) {
+    List<ImageModel> allImgs = allData[index]?.images;
+    var length = allImgs?.length;
+    if (length == 1) {
+      return new Container(
+        width: double.infinity,
+        height: 200,
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new NetworkImage(allImgs[0]?.url),
+          ),
+        ),
+      );
+    } else if (length == 0) {
+      return new Container();
+    } else {
+      return _buidGridView(allImgs);
+    }
+  }
+
+  String _getTime(int time) {
+    var daynow = DateTime.fromMillisecondsSinceEpoch(time * 1000);
+    Duration chaf = DateTime.now().difference(daynow);
+    if (chaf.inDays != 0) {
+      if (chaf.inDays > 30) {
+        return "${daynow.year}-${daynow.month}-${daynow.day}";
+      } else
+        return "${chaf.inDays}天前";
+    } else if (chaf.inHours != 0) {
+      return "${chaf.inHours}小时前";
+    } else if (chaf.inMinutes != 0) {
+      return "${chaf.inMinutes}分钟前";
+    } else if (chaf.inSeconds != 0) {
+      return "${chaf.inSeconds}刚刚";
+    }
+  }
+
+  Widget _buidGridView(List<ImageModel> imgs) {
+    return new GridView.builder(
+      shrinkWrap: true,
+      itemCount: imgs.length,
+      padding: EdgeInsets.all(5.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        crossAxisCount: imgs.length <= 3 ? imgs.length : 3,
+        childAspectRatio: 1.0,
+      ),
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return new Image(
+          image: new NetworkImage(imgs[index].url),
+          fit: BoxFit.fill,
+        );
+      },
+    );
+  }
+
+  _getListData({bool clear = false}) {
+    HttpNet().request(
+        MethodTypes.GET, "${ApiUtils.getTabs}/${widget.tabId}/post/$_pageIndex",
+        (str) {
+      NewsModel model = NewsModel.fromJson(str);
+      if (model.data == null ||
+          model.data.list == null ||
+          model.data.list.isEmpty) {
+        pullUp = false;
+        Utils.showToast("没有更多数据了");
+      } else {
+        allData.addAll(model.data.list);
+        pullUp = true;
+      }
+      _refreshController.refreshCompleted();
+      _refreshController.loadComplete();
+
+      if (mounted) setState(() {});
+    });
+  }
+}
